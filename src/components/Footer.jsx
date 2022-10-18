ba64e4e8f11ae6229bb7b764176b9dfc2bec7c70
Kalana Madushan <62831247+kalanamadushan963@users.noreply.github.com>
2022-10-18 16:46:24 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,8 +24,7 @@ function Footer() {
       </div>
       {/* detail */}
       <span className="text-[1rem] text-gray-400 px-[15rem] text-center mt-[4rem]">
-        Duis feugiat congue metus, ultrices vulputate nibh viverra eget.
-        Vestibulum ullamcorper volutpat varius.
+       Developed by Kalana Madushan
       </span>
     </div>
   );
